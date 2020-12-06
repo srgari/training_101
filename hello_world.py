7b78cc6abdcd333f6bab3ddc2e5fdd0915aa6ed2
Sergio Marinho da Silva <srgari.minion1@gmail.com>
2020-12-06 12:41:44 -0300

    master Sergio, please accept it. Sincerely, Sergio

--- hello_world.py
+++ hello_world.py
@@ -5,3 +5,8 @@ print('I can see it in your smile')
 
 list = [x for x in range(100) if x%4 and x%7 and x%13]
 print(list)
+
+print('Hi, Master Sergio. This is Sergio, your minion no.1')
+print('I am so glad to be able to assist you!')
+print('Please accept these changes to the file')
+
