1a6e0461870766fe9dab32f642ad2f612c726295
Sergio Marinho Da Silva <47604487+srgari@users.noreply.github.com>
2020-12-06 12:29:31 -0300

    Create hello_world.py
    Criei esse arquivo só como teste.
    Quero ver como o srgari-minion1 consegue clonar esse projeto, modificar e propor coisas.

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+print('hello?')
+print('is it me you are looking for?')
+print('I can see it in your eyes')
+print('I can see it in your smile')
+
+list = [x for x in range(100) if x%4 and x%7 and x%13]
+print(list)
